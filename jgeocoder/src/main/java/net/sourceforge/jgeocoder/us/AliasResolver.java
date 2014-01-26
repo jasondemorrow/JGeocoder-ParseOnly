6506647ad20bb38311836dbd4bb64e134bde4bd3
Jason DeMorrow <jasondemorrow@gmail.com>
2014-01-25 17:08:41 -0800

    removing failing code that I don't use

--- jgeocoder/src/main/java/net/sourceforge/jgeocoder/us/AliasResolver.java
+++ jgeocoder/src/main/java/net/sourceforge/jgeocoder/us/AliasResolver.java
@@ -15,6 +15,7 @@ import org.apache.commons.lang.StringUtils;
 
 class AliasResolver{
   private static final Map<String, Map<String, String>> CITY_ALIAS_MAP = new HashMap<String, Map<String,String>>();
+  /*
   static{
     BufferedReader br = null;
     try {
@@ -57,6 +58,7 @@ class AliasResolver{
       } catch (IOException e) {} }
     }
   }
+  */
   
   /**
    * @param city
